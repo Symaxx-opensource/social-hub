81e35424cfc4bcf2035a9559ecb0ebf67df481f3
Bukhosi Moyo <bukhosizimcode@gmail.com>
2021-07-07 17:59:44 +0200

    ✨  : Addig the reset urls and the dashboard

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import user_login
-from django.contrib.auth.views import LoginView, LogoutView
+from .views import user_login, dashboard
+from django.contrib.auth import views as auth_views #import LoginView, LogoutView
 
 urlpatterns = [
     #path("login/", user_login, name="login"),
-    path("login/", LoginView.as_view(), name="login"),
-    path("logout/", LogoutView.as_view(), name="logout"),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("", dashboard, name="dashboard"),
+
+    # Change passoword Urls to
+
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
